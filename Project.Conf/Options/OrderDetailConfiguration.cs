71debba38d6362fb4a282dea3a479c104b641235
hakanakinsu0 <hakanakinsu@outlook.com>
2025-05-19 23:31:29 +0300

    Configuration katmani tamamlandi.

---
+++ Project.Conf/Options/OrderDetailConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Project.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Conf.Options
+{
+    public class OrderDetailConfiguration : BaseConfiguration<OrderDetail>
+    {
+        public override void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            base.Configure(builder);
+            builder.HasIndex(x => new
+            {
+                x.OrderId,
+                x.ProductId
+            }).IsUnique();
+
+            builder.Property(x=>x.UnitPrice).HasColumnType("money");
+        }
+    }
+}
