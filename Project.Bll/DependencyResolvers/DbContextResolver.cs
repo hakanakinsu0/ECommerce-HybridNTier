8ee788a7e80d548957557e6d066fce667d712bac
hakanakinsu0 <hakanakinsu@outlook.com>
2025-05-26 01:06:11 +0300

    Bll katmaninda DependencyResolver klasoru acilip, middleware icin baglanti ayarlamalari ve identity static classlari acildi.

---
+++ Project.Bll/DependencyResolvers/DbContextResolver.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project.Dal.ContextClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Bll.DependencyResolvers
+{
+    public static class DbContextResolver
+    {
+        public static void AddDbContextService(this IServiceCollection services)
+        {
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            IConfiguration configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            services.AddDbContext<MyContext>(x => x.UseSqlServer(configuration.GetConnectionString("MyConnection")).UseLazyLoadingProxies());
+        }
+    }
+}
---
+++ Project.Bll/DependencyResolvers/IdentityResolver.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.Dal.ContextClasses;
+using Project.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Bll.DependencyResolvers
+{
+    public static class IdentityResolver
+    {
+        public static void AddIdentityService(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser,AppRole>(x=>
+            {
+                x.Password.RequireDigit = false;
+                x.Password.RequiredLength = 3;
+                x.Password.RequireLowercase = false;
+                x.Password.RequireUppercase = false;
+                x.SignIn.RequireConfirmedEmail = true;
+                x.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<MyContext>();
+        }
+    }
+}
