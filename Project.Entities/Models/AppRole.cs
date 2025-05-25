7089c471d3c5960f3618f808a00cf3ef7355fd55
hakanakinsu0 <hakanakinsu@outlook.com>
2025-05-26 00:51:34 +0300

    Entities katmaninda Model klasoru icerisine yeni class'lar eklendi.

--- Project.Entities/Models/AppRole.cs
+++ Project.Entities/Models/AppRole.cs
@@ -9,20 +9,14 @@ using System.Threading.Tasks;
 
 namespace Project.Entities.Models
 {
-    public class AppUser : IdentityUser<int>, IEntity
+    public class AppRole : IdentityRole<int>, IEntity
     {
-        public Guid ActivationCode { get; set; }
-        public bool IsBanned { get; set; }
-
         public DateTime CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
         public DataStatus Status { get; set; }
 
         //Relational Properties
-        public virtual AppUserProfile Profile { get; set; }
-        public virtual ICollection<AppUserRole> UserRoles { get; set; }
-        public virtual ICollection<Order> Orders { get; set; }
-
+        public virtual ICollection<AppUserRole> AppUserRoles { get; set; }
     }
 }
--- Project.Entities/Models/AppUserRole.cs
+++ Project.Entities/Models/AppUserRole.cs
@@ -9,20 +9,20 @@ using System.Threading.Tasks;
 
 namespace Project.Entities.Models
 {
-    public class AppUser : IdentityUser<int>, IEntity
+    public class AppUserRole : IdentityUserRole<int>, IEntity
     {
-        public Guid ActivationCode { get; set; }
-        public bool IsBanned { get; set; }
-
+        public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
         public DataStatus Status { get; set; }
 
-        //Relational Properties
-        public virtual AppUserProfile Profile { get; set; }
-        public virtual ICollection<AppUserRole> UserRoles { get; set; }
-        public virtual ICollection<Order> Orders { get; set; }
+        //Foreign Key 
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
 
+        //Relational Properties
+        public virtual AppUser User { get; set; }
+        public virtual AppRole Role { get; set; }
     }
 }
