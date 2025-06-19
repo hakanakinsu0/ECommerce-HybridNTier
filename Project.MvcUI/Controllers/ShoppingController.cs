594f2338055b6fde2a4a2462db1801a1b3941142
hakanakinsu0 <hakanakinsu@outlook.com>
2025-06-19 21:59:51 +0300

    Alisveris sistemi entegre edildi. Refaktorlere baslanacak.

--- Project.MvcUI/Controllers/ShoppingController.cs
+++ Project.MvcUI/Controllers/ShoppingController.cs
@@ -14,7 +14,7 @@ namespace Project.MvcUI.Controllers
     public class ShoppingController : Controller
     {
         readonly IProductManager _productManager;
-        readonly ICategoryManager _categoryManager; 
+        readonly ICategoryManager _categoryManager;
         readonly IOrderManager _orderManager;
         readonly IOrderDetailManager _orderDetailManager;
         readonly UserManager<AppUser> _userManager;
@@ -34,7 +34,7 @@ namespace Project.MvcUI.Controllers
 
         public async Task<IActionResult> Index(int? page, int? categoryId)
         {
-            List<Product> productList = categoryId==null? await _productManager.GetAllAsync() :  _productManager.Where(x=>x.CategoryId==categoryId).ToList();
+            List<Product> productList = categoryId == null ? await _productManager.GetAllAsync() : _productManager.Where(x => x.CategoryId == categoryId).ToList();
 
             IPagedList<Product> pagedProducts = productList.ToPagedList(page ?? 1, 5);
 
@@ -67,7 +67,6 @@ namespace Project.MvcUI.Controllers
             if (c.GetCartItems.Count == 0) HttpContext.Session.Remove("scart");
         }
 
-
         #endregion
 
         public async Task<IActionResult> AddToCart(int id)
@@ -91,7 +90,7 @@ namespace Project.MvcUI.Controllers
             SetCartForSession(c);
             TempData["Message"] = $"{ci.ProductName} isimli ürün sepete eklenmiştir";
             return RedirectToAction("Index");
-        
+
         }
 
         public IActionResult CartPage()
@@ -103,6 +102,90 @@ namespace Project.MvcUI.Controllers
             }
             Cart c = GetCartFromSession("scart");
             return View(c);
+        }
+
+
+        //ToDo : Refaktor edilmesi lazim. Benzer kodlar var.
+        public IActionResult RemoveFromCart(int id)
+        {
+            if (GetCartFromSession("scart") != null)
+            {
+                Cart c = GetCartFromSession("scart");
+                c.RemoveFromCart(id);
+                SetCartForSession(c);
+                ControlCart(c);
+            }
+
+            return RedirectToAction("CartPage");
+        }
+
+        public IActionResult DecreaseFromCart(int id)
+        {
+            if (GetCartFromSession("scart") != null)
+            {
+                Cart c = GetCartFromSession("scart");
+                c.Decrease(id);
+                SetCartForSession(c);
+                ControlCart(c);
+            }
+            return RedirectToAction("CartPage");
+        }
+
+        public IActionResult IncreaseCartItem(int id)
+        {
+            if (GetCartFromSession("scart") != null)
+            {
+                Cart c = GetCartFromSession("scart");
+                c.IncreaseCartItem(id);
+                SetCartForSession(c);
+                ControlCart(c);
+            }
+            return RedirectToAction("CartPage");
+        }
+
+        public IActionResult ConfirmOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConfirmOrder(OrderRequestPageVm ovm)
+        {
+            Cart c = GetCartFromSession("scart");
+            ovm.Order.Price = c.TotalPrice;
+            #region APIBankEntegrasyonu
+
+            #endregion
+
+            if (User.Identity.IsAuthenticated)
+            {
+                AppUser appUser = await _userManager.FindByNameAsync(User.Identity.Name);
+                ovm.Order.AppUserId = appUser.Id;
+            }
+
+            await _orderManager.CreateAsync(ovm.Order);
+
+            foreach (CartItem item in c.GetCartItems)
+            {
+                OrderDetail orderDetail = new();
+                orderDetail.OrderId = ovm.Order.Id;
+                orderDetail.ProductId = item.Id;
+                orderDetail.Amount = item.Amount;
+                orderDetail.UnitPrice = item.UnitPrice;
+
+                await _orderDetailManager.CreateAsync(orderDetail);
+
+                //ToDo : Urun stok miktarini azaltma islemi yapilacak. Urun update edilecek.
+
+
+
+
+
+            }
+
+            TempData["Message"] = "Siparisiniz alinmistir. Teşekkür ederiz.";
+            HttpContext.Session.Remove("scart"); //Sepeti bosaltma islemi yapildi.
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Project.MvcUI/Models/PageVms/OrderRequestPageVm.cs
@@ -0,0 +1,9 @@
+﻿using Project.Entities.Models;
+
+namespace Project.MvcUI.Models.PageVms
+{
+    public class OrderRequestPageVm
+    {
+        public Order Order { get; set; }
+    }
+}
